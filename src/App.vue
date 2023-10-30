43ba700d2a71364ef2b51f928c55e0150fbf34c7
Christopher Graham <chgraham@springfieldclinic.com>
2023-10-30 10:58:34 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+	<div class="flex flex-col justify-center items-center pt-10">
+		<img class="w-96 h-96" alt="Lemonato logo" src="./assets/lemonato_logo.png" />
+		<h1 class="text-3xl font-bold text-pod mt-10">
+			Lemonato
+		</h1>
+	</div>
+</template>
+
+<style scoped></style>
